f3c60c428e7333c71588dc873ffd98790ec76a50
kuldeeepy <kuldeeep.yadav1@gmail.com>
2024-11-25 12:51:47 +0530

    fine tuning

--- public/scripts/movie.mjs
+++ public/scripts/movie.mjs
@@ -1,14 +1,14 @@
-import getMovie from './index.mjs'
+import getMovie from "./index.mjs";
 
 let loggedUser = JSON.parse(localStorage.getItem("user"));
 
-let userIcon = document.querySelector(".user")
+let userIcon = document.querySelector(".user");
 
 if (!loggedUser) {
-    window.location.href = './login.html'
+  window.location.href = "./login.html";
 } else {
-    userIcon.classList.remove("fa-regular", "fa-user")
-    userIcon.classList.add("fa-solid", "fa-user")
+  userIcon.classList.remove("fa-regular", "fa-user");
+  userIcon.classList.add("fa-solid", "fa-user");
 }
 
 let param = new URLSearchParams(window.location.search);
@@ -16,138 +16,148 @@ let movieID = param.get("movie_id");
 
 let token = window.env.API_TOKEN;
 // let key = window.env.API_KEY;
-let imageUrl = 'https://image.tmdb.org/t/p/original/';
+let imageUrl = "https://image.tmdb.org/t/p/original/";
 
-let seriesVideo = `https://api.themoviedb.org/3/tv/${movieID}/videos?language=en-US`; 
-let movieVideo = `https://api.themoviedb.org/3/movie/${movieID}/videos?language=en-US`; 
+let seriesVideo = `https://api.themoviedb.org/3/tv/${movieID}/videos?language=en-US`;
+let movieVideo = `https://api.themoviedb.org/3/movie/${movieID}/videos?language=en-US`;
 
-let seriesInfo = `https://api.themoviedb.org/3/tv/${movieID}?language=en-U`
-let movieInfo = `https://api.themoviedb.org/3/movie/${movieID}?language=en-US`
-
+let seriesInfo = `https://api.themoviedb.org/3/tv/${movieID}?language=en-U`;
+let movieInfo = `https://api.themoviedb.org/3/movie/${movieID}?language=en-US`;
 
 let container = document.getElementById("container");
 
-getContentById()
+getContentById();
 
 async function getContentById() {
+  let ops = {
+    method: "GET",
+    headers: {
+      accept: "application/json",
+      Authorization: `Bearer ${token}`,
+    },
+  };
 
-    let ops = {
-        method: "GET",
-        headers: {
-            accept: "application/json",
-            Authorization: `Bearer ${token}`
-        }
-    };
+  try {
+    let seriesChek = await fetch(seriesVideo, ops);
 
-    try {
-
-        let seriesChek = await fetch(seriesVideo, ops);
-
-        if (seriesChek.status === 200) {
-            return await getSeriesContent(ops)
-        }
-
-        let movieCheck = await fetch(movieVideo, ops);
-
-        if (movieCheck.status === 200) {
-            return await getMovieContent(ops)
-        }
-        
-    } catch (error) {
-        alert("Sorry content not found, Redirecting you back in 2s")
-        console.error("Sorry content not found, Redirecting you back in 2s")
-
-        setTimeout(() => {
-            window.location.href = './index.html'
-        }, 2000)
+    if (seriesChek.status === 200) {
+      return await getSeriesContent(ops);
     }
+
+    let movieCheck = await fetch(movieVideo, ops);
+
+    if (movieCheck.status === 200) {
+      return await getMovieContent(ops);
+    }
+  } catch (error) {
+    alert("Sorry content not found, Redirecting you back in 2s");
+    console.error("Sorry content not found, Redirecting you back in 2s");
+
+    setTimeout(() => {
+      window.location.href = "./index.html";
+    }, 2000);
+  }
 }
 
 async function getMovieContent(ops) {
+  let resp = await fetch(movieVideo, ops);
+  let video = await resp.json();
 
-    let resp = await fetch(movieVideo, ops);
-    let video = await resp.json();
+  if (!video.results && video.results.length == 0) {
+    return new Error("No video found!");
+  }
 
-    if (!video.results && video.results.length == 0) {
-        return new Error("No video found!")
-    }    
+  // console.log(video.results);
 
-    // console.log(video.results);
-    
-    let iteration = 20;
-    let key = undefined;
+  let iteration = 20;
+  let key = undefined;
 
-    for (let idx = 0; idx < Math.min(iteration, video.results.length); idx++) {
-        if (video.results[idx].name.toLowerCase().includes('trailer') && video.results[idx].key) {
-            key = video.results[idx].key;
-            break; 
-        }
+  for (let idx = 0; idx < Math.min(iteration, video.results.length); idx++) {
+    if (
+      video.results[idx].name.toLowerCase().includes("trailer") &&
+      video.results[idx].key
+    ) {
+      key = video.results[idx].key;
+      break;
     }
-    
-    // let key = video.results[0].key || video.results[1].key || null;
+  }
 
-    let res = await fetch(movieInfo, ops);
-    let info = await res.json();  
+  // let key = video.results[0].key || video.results[1].key || null;
 
-    const movieContent = {
-        name: info.original_title,
-        video: key,
-        imageOne: info.backdrop_path,
-        imageTwo: info.poster_path,
-        releasedOn: info.release_date,
-        no_of_seasons: info.number_of_seasons,
-        brief: info.overview,
-        genre: info.genres.map(genre => genre.name).join(', ') || 'N/A',
-        avg_rating: info.vote_average,
-        production: info.production_companies.map(prod => prod.name).join(', ') || 'N/A',
-        lang: info.spoken_languages.length > 0 ? info.spoken_languages[0]?.english_name : 'N/A',
-        origin: info.production_countries.length > 0 ? info.production_countries[0].name : 'N/A',
-    };
+  let res = await fetch(movieInfo, ops);
+  let info = await res.json();
 
-    displayContent(movieContent);
+  const movieContent = {
+    name: info.original_title,
+    video: key,
+    imageOne: info.backdrop_path,
+    imageTwo: info.poster_path,
+    releasedOn: info.release_date,
+    no_of_seasons: info.number_of_seasons,
+    brief: info.overview,
+    genre: info.genres.map((genre) => genre.name).join(", ") || "N/A",
+    avg_rating: info.vote_average,
+    production:
+      info.production_companies.map((prod) => prod.name).join(", ") || "N/A",
+    lang:
+      info.spoken_languages.length > 0
+        ? info.spoken_languages[0]?.english_name
+        : "N/A",
+    origin:
+      info.production_countries.length > 0
+        ? info.production_countries[0].name
+        : "N/A",
+  };
+
+  displayContent(movieContent);
 }
 
 async function getSeriesContent(ops) {
+  let resp = await fetch(seriesVideo, ops);
+  let video = await resp.json();
 
-    let resp = await fetch(seriesVideo, ops);
-    let video = await resp.json();
+  if (!video.results && video.results.length == 0) {
+    return new Error("No video found!");
+  }
 
-    if (!video.results && video.results.length == 0) {
-        return new Error("No video found!")
-    }    
-    
-    let key = video.results[0].key || video.results[1].key || null;
+  let key = video.results[0].key || video.results[1].key || null;
 
-    let res = await fetch(seriesInfo, ops);
-    let info = await res.json();
-    
-    const tvContent = {
-        name: info.name,
-        video: key,
-        imageOne: info.backdrop_path,
-        imageTwo: info.poster_path,
-        releasedOn: info.first_air_date,
-        no_of_seasons: info.number_of_seasons,
-        brief: info.overview,
-        genre: info.genres.map(genre => genre.name).join(', ') || 'N/A',
-        avg_rating: info.vote_average,
-        production: info.production_companies.map(prod => prod.name).join(', ') || 'N/A',
-        lang: info.spoken_languages.length > 0 ? info.spoken_languages[0]?.english_name : 'N/A',
-        origin: info.production_countries.length > 0 ? info.production_countries[0].name : 'N/A',
-    };
+  let res = await fetch(seriesInfo, ops);
+  let info = await res.json();
 
-    displayContent(tvContent);
+  const tvContent = {
+    name: info.name,
+    video: key,
+    imageOne: info.backdrop_path,
+    imageTwo: info.poster_path,
+    releasedOn: info.first_air_date,
+    no_of_seasons: info.number_of_seasons,
+    brief: info.overview,
+    genre: info.genres.map((genre) => genre.name).join(", ") || "N/A",
+    avg_rating: info.vote_average,
+    production:
+      info.production_companies.map((prod) => prod.name).join(", ") || "N/A",
+    lang:
+      info.spoken_languages.length > 0
+        ? info.spoken_languages[0]?.english_name
+        : "N/A",
+    origin:
+      info.production_countries.length > 0
+        ? info.production_countries[0].name
+        : "N/A",
+  };
+
+  displayContent(tvContent);
 }
 
 function displayContent(movie) {
+  let mainPage = document.createElement("div");
+  mainPage.className = "page";
 
-    let mainPage = document.createElement("div");
-    mainPage.className = 'page'
-    
-    let videoCard = document.createElement("div");
-    videoCard.setAttribute("id", 'video')
+  let videoCard = document.createElement("div");
+  videoCard.setAttribute("id", "video");
 
-    videoCard.innerHTML = `
+  videoCard.innerHTML = `
         <iframe 
             width="560" 
             height="315" 
@@ -157,15 +167,17 @@ function displayContent(movie) {
             allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; fullscreen" 
             allowfullscreen>
         </iframe>
-    `
+    `;
 
-    mainPage.appendChild(videoCard);
+  mainPage.appendChild(videoCard);
 
-    mainPage.innerHTML += `
+  mainPage.innerHTML += `
     
         <div>
             <div class='details'>
-            <img src="${imageUrl + movie.imageTwo || imageUrl + movie.imageOne}" />
+            <img src="${
+              imageUrl + movie.imageTwo || imageUrl + movie.imageOne
+            }" />
             <div>
                 <h1>${movie.name}</h1>
                 <div>
@@ -180,52 +192,55 @@ function displayContent(movie) {
             </div>
             </div>
         </div>
-    `
-    container.appendChild(mainPage);
+    `;
+  container.appendChild(mainPage);
 
-    recommendation();
+  recommendation();
 }
 
 async function recommendation() {
+  let ops = {
+    method: "GET",
+    headers: {
+      accept: "application/json",
+      Authorization: `Bearer ${token}`,
+    },
+  };
 
-    let ops = {
-        method: "GET",
-        headers: {
-            accept: "application/json",
-            Authorization: `Bearer ${token}`
-        }
-    };
+  const details = document.querySelector(".page");
 
-    const details = document.querySelector(".page");
+  let recommends = document.createElement("div");
+  recommends.className = "recommends";
 
-    let recommends = document.createElement("div");
-    recommends.className = 'recommends';
+  let resp = await fetch(
+    `https://api.themoviedb.org/3/tv/top_rated?language=en-US&page=${Math.floor(
+      Math.random() * 100
+    )}`,
+    ops
+  );
+  let data = await resp.json();
 
-    let resp = await fetch(`https://api.themoviedb.org/3/tv/top_rated?language=en-US&page=${Math.floor(Math.random() * 100)}`, ops);
-    let data = await resp.json();
+  const movies = data.results.slice(0, 8);
 
-    const movies = data.results.slice(0, 8);
+  movies.forEach((movie) => {
+    let card = document.createElement("div");
 
-    movies.forEach((movie) => {
-        let card = document.createElement("div");
+    let title = movie.name.split(":")[0];
 
-        let title = movie.name.split(":")[0];
+    title.length >= 24 ? (title = title.slice(0, 19)) : title;
 
-        title.length >= 24 ? title = title.slice(0,19) : title;
-        
-        card.innerHTML += `
-        <div>
-            <img src="${imageUrl + movie.poster_path}" alt="${movie.name}" loading="lazy"/>
-            <p>${title}</p>
-            <div class="info">
-                <h2>${movie.first_air_date.slice(0,4)}</h2>
-                <p>&starf; ${movie.vote_average.toFixed(1)}</p>
-            </div>
-        </div>
-        `;
-        card.addEventListener("click", getMovie.bind(null, movie.id));
-        recommends.appendChild(card);
-    });
+    card.innerHTML += `
+    <img src="${imageUrl + movie.poster_path}" alt="${
+      movie.name
+    }" loading="lazy"/>
+    <p>${title}</p>
+    <div class="info">
+        <h2>${movie.first_air_date.slice(0, 4)}</h2>
+        <p>&starf; ${movie.vote_average.toFixed(1)}</p>
+    </div>`;
+    card.addEventListener("click", getMovie.bind(null, movie.id));
+    recommends.appendChild(card);
+  });
 
-    details.appendChild(recommends);
+  details.appendChild(recommends);
 }
